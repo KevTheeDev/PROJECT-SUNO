e0e2a25ccdc9a42a1c9d6a89cb3f1c43ae7f3be2
KevinJ.Regis <dev.regis1@gmail.com>
2020-03-12 16:50:16 -0500

    emergency  submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // **************** SUNO *********************
-console.log(`\n \n \n S-UNO \n `)
+// console.log(`\n \n \n S-UNO \n `)
+
+
 
 // create an two objects with cards
 //         //  these are the player's hand
@@ -13,43 +15,62 @@ console.log(`\n \n \n S-UNO \n `)
 //                 // cards
 //                     green,
 //                     red,
-//                     yellow,
+//                      yellow,
 
 //             // a loop
 //                 loop through each object 
+// import { Object, Math } from "es6-shim";
+let round = 0
+
+let randomNum = parseInt(Math.random() * 11)
+console.log(randomNum);
 
 
 let player1 = {
-    green: [3, 3, 4],
-    red: 3,
-    blue: 4,
+    cards: randomNum,
+    wins: 0
 }
 
 let player2 = {
-    green: [3, 2, 1],
-    // may need to use a sort method
-    red: '3',
-    blue: '4',
+    cards: randomNum,
+    wins: 0
 }
+let x = Object.values(player2);
+
+// write a function that does the following:
+// - initialize a variable whose value is a random num (using math.random)
+// - push random number to player.cards
+
+// write a function that check if each index of player1.cards[round] 
+// matches the same index of player2.cards[round]. whoever has the higher card, increment their wins.
+
+// write a function called "battle" that calls each of these functions to similate a game. at the end of the function, increment the round.
+
+//put maht.random in a function
 
 function loopPlay() {
     for (let player1Loop in player1) {
-        console.log(player1.green)
+        console.log(player1.green, '\n')
     }
 }
 loopPlay();
 
 
 function loopHand1() {
-    for (let hand1 in player1) {
-        for (let hand2 in player2) {
-            if (player1.green[0] == player2.green[0]) {
-                player2.green.pop(0)
-                console.log()
-            }
+    for (let color in player2) {
+        for (let item in player2.color) {
+            console.log(item);
+            // if (player1.green[0] == player2.green[0]) {
+            //     console.log(`loopHand1 `, player2.green[0])
+            // } else {
+            //     console.log(`if statement not working`)
+            // }
         }
     }
 }
+loopHand1();
+
+
 
 // creates an array of the KEYS out of the object
 // let key = Object.keys(player2);
