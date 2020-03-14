720f97056e141151c510cf1de5320c90f56fd381
KevinJ.Regis <dev.regis1@gmail.com>
2020-03-13 20:31:23 -0500

    added functions for game play
    func for both player

--- pseudoCode/psuedo.js
+++ pseudoCode/psuedo.js
@@ -1,32 +1,70 @@
-// <!-- two players
-//      1. both players throw down card until there are no more cards in one of the player's hand
-//         // create an two objects with cards
-//         //  these are the player's hand
-//             // 1st Object
-//                 // cards:
-//                         green,
-//                         red,
-//                         yellow,
+// <!--           
+// write a function that does the following:
+// - initialize a variable whose value is a random num (using math.random)
+// - push random number to player.cards
 
-//             // 2nd Object
-//                 // cards
-//                     green,
-//                     red,
-//                     yellow,
+// write a function that check if each index of player1.cards[round] 
+// matches the same index of player2.cards[round]. whoever has the higher card, increment their wins.
 
-//             // a loop
-//                 loop through each object 
-
-//             // To Begin
-//                 // have a function to loop through each player's hand[ 1st object] until the output of both match 
-//                     // use an if statement to see if the results match
-//                     // if they do then output 'they match'
-//                         //...
-
-//      2. the cards either player throws down must match the card previously thrown down
-
-//      3. if the player does not have a matching card then a new card is added to their hand
-//         // this is an array, a new index is added to this players array
-//             // if there are no matching cards in the master array            
+// write a function called "battle" that calls each of these functions to similate a game. at the end of the function, increment the round.
 
 // -->
+
+
+
+
+
+
+// creates an array of the KEYS out of the object
+// let key = Object.keys(player2);
+// console.log(key);
+
+//prints the values nad makes them arrays
+// let val = Object.values(player2);
+// console.log(`Value Method `, val);
+
+//prints a table of the entries in the object
+// let x = Object.entries(player1)
+
+
+
+
+
+
+
+
+
+
+
+// ************************* TESTING FUNCTIONS WORKED ***************************
+// function testPlayer() {
+
+//     let player1 = {
+//         green: 2,
+//         red: 3,
+//         blue: 4,
+//     }
+//     console.log(player1.green)
+
+// }
+// testPlayer();
+
+
+// function testPlayer2() {
+
+//     let player2 = {
+//         green: 2,
+//         red: 3,
+//         blue: 4,
+//     }
+//     console.log(player2.red)
+// }
+// testPlayer2();
+
+
+// function testHands() {
+//     if (testPlayer() == testPlayer2()) {
+//         console.log(`They matched! `)
+//     }
+// }
+// testHands();
--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // **************** SUNO *********************
 console.log(`\n \n \n WAR-S \n `)
-
 let round = 0
 
 function generateNum(){
@@ -10,31 +9,52 @@ return randomNum;
 
 let player1Cards = {
     cards: generateNum(),
-    wins: 0
 }
 
 let player2Cards = {
     cards: generateNum(),
-    wins: function (){
-        let wins = 0;
-        
-    }
 }
 
-console.log(player1Cards.cards)
-console.log(player2Cards.cards)
 
 
 function checkCards (){
-    if (player1Cards.cards < player2Cards.cards){
+    let Player1Wins=0;
+    let Player2Wins=0
+    let gameIsWon=false;
+
+    while (!gameIsWon) {
+        if (player1Cards.cards > player2Cards.cards){
+            console.log(` Player 1: ${player1Cards.cards} `)
+            Player1Wins++;
+            //alert and maybe a prompt
+            console.log(` PLAYER ONE wins this round \n Play again? Enter yes: \n Exit: enter 'N'`)
+            //if yes then update random nums update objects
+            // if no then break
             
-        console.log(`Player one wins this round`)
-    } else if (player2Cards.cards < player1Cards.cards){
-        console.log(`Player 2 wins this round`)
+        } else if (player2Cards.cards > player1Cards.cards){
+            console.log(` Player 2: ${player2Cards.cards} \n `)
+            // alert and maybe A prompt
+            Player2Wins++;
+            console.log(` PLAYER TWO wins this round \n Play again? Enter yes: \n Exit: enter 'N'`)
+            //if yes then update random nums update objects
+            // if no then break
+            
+        } else if (player1Cards.cards == player2Cards.cards){
+            //alert 
+            console.log(`ROUND IS A DRAW: Try again? Enter yes:`)
+            //if yes then update random nums update objects
+            // if no then break
+        }
+        gameIsWon=((Player1Wins>=5) || (Player2Wins>=5));
     }
-
 }
+checkCards();
 
+
+
+
+
+// console.log(player2Cards.this.wins());
 
 // write a function that does the following:
 // - initialize a variable whose value is a random num (using math.random)
